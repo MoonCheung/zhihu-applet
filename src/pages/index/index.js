fd8c12546da760bc0af84e208e86e23d1399ff10
MoonCheung <Salvador23@163.com>
2019-08-02 23:42:27 +0800

    updating index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -23,7 +23,25 @@ class Index extends Taro.Component {
         column: '去往专栏',
         live: '全部live'
       },
-      status: 'noMore'
+      scrollY: true,
+      // 拖动上下滚动
+      dragStyle: {
+        top: 0 + 'px'
+        // bottom: 0 + 'px'
+      },
+      //下拉样式
+      downPullStyle: {
+        height: 0 + 'px'
+      },
+      downPullText: '下拉刷新',
+      //上拉样式
+      // upPullStyle: {
+      //   height: 0 + 'px'
+      // },
+      // upPullText: '上拉加载更多',
+      creState: {},
+      pullState: 0, //刷新状态 0不做操作 1刷新 -1加载更多
+      status: 'more'
     };
   }
 
@@ -116,6 +134,150 @@ class Index extends Taro.Component {
       isActive: index
     });
   };
+  // 还原初始设置
+  reduction = () => {
+    let time = 0.5;
+    let that = this;
+    that.setState({
+      dragStyle: {
+        top: 0 + 'px',
+        // bottom: 0 + 'px',
+        transition: `all ${time}s`
+      },
+      // upPullStyle: {
+      //   height: 0 + 'px',
+      //   transition: `all ${time}s`
+      // },
+      downPullStyle: {
+        height: 0 + 'px',
+        transition: `all ${time}s`
+      },
+      pullState: 0,
+      scrollY: true
+    });
+    setTimeout(() => {
+      that.setState({
+        dragStyle: {
+          top: 0 + 'px'
+          // bottom: 0 + 'px'
+        },
+        // upPullStyle: {
+        //   height: 0 + 'px'
+        // },
+        downPullStyle: {
+          height: 0 + 'px'
+        },
+        downPullText: '下拉刷新',
+        upPullText: '上拉加载更多'
+      });
+    }, time * 1000);
+  };
+  touchStart = e => {
+    console.log(`touchStart`);
+    let that = this;
+    that.setState({
+      creState: e.touches[0]
+    });
+  };
+  touchMove = e => {
+    console.log(`touchMove`);
+    e.stopPropagation();
+    let that = this;
+    let move = e.touches[0]; //移动时的位置
+    let deviationX = 0.3; //左右偏移量(超过这个偏移量不执行下拉操作)
+    let deviationY = 70; //拉动长度（低于这个值的时候不执行）
+    let maxY = 100; //拉动的最大高度
+
+    let start_x = that.state.creState.clientX;
+    let start_y = that.state.creState.clientY;
+    let move_x = move.clientX;
+    let move_y = move.clientY;
+
+    //得到偏移数值
+    let dev = Math.abs(move_x - start_x) / Math.abs(move_y - start_y);
+    //当偏移数值大于设置的偏移数值时则不执行操作
+    if (dev < deviationX) {
+      console.log(`下拉操作`, move_y - start_y);
+      console.log(`上拉操作`, start_y - move_y);
+      //拖动倍率
+      let dragY = Math.abs(move_y - start_y) / 3.5;
+      console.log(`dragY`, dragY);
+      //下拉操作
+      if (move_y - start_y > 0) {
+        if (dragY >= deviationY) {
+          this.setState({
+            pullState: 1,
+            downPullText: '释放刷新'
+          });
+        } else {
+          this.setState({
+            pullState: 0,
+            downPullText: '下拉刷新'
+          });
+        }
+        if (dragY >= maxY) {
+          dragY = maxY;
+        }
+        this.setState({
+          dragStyle: {
+            top: dragY + 'px'
+          },
+          downPullStyle: {
+            height: dragY + 'px'
+          },
+          scrollY: false
+        });
+      }
+      // 上拉操作
+      // if (start_y - move_y > 0) {
+      //   if (dragY >= deviationY) {
+      //     this.setState({
+      //       pullState: -1,
+      //       upPullText: '释放加载更多'
+      //     });
+      //   } else {
+      //     this.setState({
+      //       pullState: 0,
+      //       upPullText: '上拉加载更多'
+      //     });
+      //   }
+      //   if (dragY >= maxY) {
+      //     dragY = maxY;
+      //   }
+      //   this.setState({
+      //     dragStyle: {
+      //       top: -dragY + 'px'
+      //     },
+      //     upPullStyle: {
+      //       height: dragY + 'px'
+      //     },
+      //     scrollY: false
+      //   });
+      // }
+    }
+  };
+  touchEnd = () => {
+    console.log(`touchEnd`);
+    let that = this;
+    that.reduction();
+  };
+
+  //页面上拉触底事件的处理函数
+  onReachBottom = () => {
+    if (!this.state.isShow && !this.state.isShowQues) {
+      switch (+this.state.isActive) {
+        case 1:
+          this.getMoreRecList();
+          break;
+        case 2:
+          this.getMoreHotList();
+          break;
+        default:
+          return;
+      }
+    }
+  };
+
   // 获得关注列表API
   getFocusList = flag => {
     let that = this;
@@ -190,43 +352,45 @@ class Index extends Taro.Component {
   };
   // 获取热榜列表API
   getHotList = flag => {
-    let that = this;
-    api.http('hotListApi', {}, res => {
-      if (res.errMsg) {
-        util.showModel(res.errMsg);
-      } else {
-        if (!flag) {
-          console.log('---设置数据---');
-          that.setState({ hotList: res.list });
-        }
-        if (flag) {
-          that.setState({
-            hotList: res.list.concat(that.state.hotList)
-          });
-          console.log('---刷新热门列表数据---');
-          Taro.stopPullDownRefresh();
-          Taro.hideNavigationBarLoading();
-          util.showSuccess(res.list.length + '条新内容');
-        }
-      }
-    });
+    // let that = this;
+    // api.http('hotListApi', {}, res => {
+    //   if (res.errMsg) {
+    //     util.showModel(res.errMsg);
+    //   } else {
+    //     if (!flag) {
+    //       console.log('---设置数据---');
+    //       that.setState({ hotList: res.list });
+    //     }
+    //     if (flag) {
+    //       that.setState({
+    //         hotList: res.list.concat(that.state.hotList)
+    //       });
+    //       console.log('---刷新热门列表数据---');
+    //       Taro.stopPullDownRefresh();
+    //       Taro.hideNavigationBarLoading();
+    //       util.showSuccess(res.list.length + '条新内容');
+    //     }
+    //   }
+    // });
+    console.log('滚动到顶部事件');
   };
   // 获取更多热榜列表
   getMoreHotList = () => {
-    let that = this;
-    that.setState({
-      status: 'loading'
-    });
-    setTimeout(() => {
-      api.http('hotListApi', {}, res => {
-        !res.errorMsg
-          ? that.setState({ hotList: that.state.hotList.concat(res.list) })
-          : util.showModel(res.errMsg);
-        that.setState({
-          status: 'more'
-        });
-      });
-    }, 500);
+    // let that = this;
+    // that.setState({
+    //   status: 'loading'
+    // });
+    // setTimeout(() => {
+    //   api.http('hotListApi', {}, res => {
+    //     !res.errorMsg
+    //       ? that.setState({ hotList: that.state.hotList.concat(res.list) })
+    //       : util.showModel(res.errMsg);
+    //     that.setState({
+    //       status: 'more'
+    //     });
+    //   });
+    // }, 500);
+    console.log('滚动到底部事件');
   };
 
   goTitleDetail = event => {
@@ -275,10 +439,17 @@ class Index extends Taro.Component {
       recList,
       footerTip,
       hotList,
-      status
+      scrollY,
+      dragStyle,
+      downPullStyle,
+      downPullText
+      // upPullStyle,
+      // upPullText
     } = this.state;
     const tabList = [{ title: '关注' }, { title: '推荐' }, { title: '热榜' }];
-    const Threshold = 50;
+    const upperThreshold = 50;
+    const lowerThreshold = 50;
+    const scrollAnimation = true;
     return (
       <View className="container">
         <View className="search-wrap">
@@ -419,10 +590,11 @@ class Index extends Taro.Component {
             <ScrollView
               scrollY
               className={'tab-container ' + (isActive == 1 ? 'show' : 'hide')}
-              upperThreshold={Threshold}
-              lowerThreshold={Threshold}
+              upperThreshold={upperThreshold}
+              lowerThreshold={lowerThreshold}
               onScrollToUpper={this.getRecommendList}
-              onScrollToLower={this.getMoreRecList}>
+              onScrollToLower={this.getMoreRecList}
+              scrollWithAnimation>
               <View className="tab-content">
                 {recList.map((item, index) => {
                   return (
@@ -469,44 +641,70 @@ class Index extends Taro.Component {
           </AtTabsPane>
           {/* 热榜内容 */}
           <AtTabsPane current={isActive} index={2}>
-            <ScrollView
-              scrollY
-              className={'tab-container ' + (isActive == 2 ? 'show' : 'hide')}
-              upperThreshold={Threshold}
-              lowerThreshold={Threshold}
-              onScrollToUpper={this.getHotList}
-              onScrollToLower={this.getMoreHotList}>
-              <View className="tab-content">
-                {hotList.map((item, index) => {
+            <View className="dragUpdatePage">
+              <View className="downDragBox" style={downPullStyle}>
+                <AtActivityIndicator content={downPullText}></AtActivityIndicator>
+              </View>
+              <ScrollView
+                style={dragStyle}
+                scrollY={scrollY}
+                className={'tab-container ' + (isActive == 2 ? 'show' : 'hide')}
+                onTouchStart={this.touchStart}
+                onTouchMove={this.touchMove}
+                onTouchEnd={this.touchEnd}
+                upperThreshold={upperThreshold}
+                lowerThreshold={lowerThreshold}
+                onScrollToUpper={this.getHotList}
+                onScrollToLower={this.onReachBottom}
+                scrollWithAnimation={scrollAnimation}>
+                <View className="tab-content">
+                  {/* {hotList.map((item, index) => {
                   return (
-                    <View>
-                      <View className="at-row tab-content-hot" key={index}>
-                        <View className="at-col at-col-1">
-                          <Text className={'hot-index ' + (index < 3 ? 'hot-index-hot' : '')}>
-                            {index + 1}
-                          </Text>
-                        </View>
-                        <View className="at-col at-col-8 at-col--wrap">
-                          <Text className="hot-title">{item.title}</Text>
-                          <View className="hot-footer-text">
-                            <Text>{item.comment + '回答 · ' + item.focus + '关注'}</Text>
-                          </View>
-                        </View>
-                        <View className="at-col at-col-3">
-                          <Image className="hot-image" src={item.image} />
+                    <View className="at-row tab-content-hot" key={index}>
+                      <View className="at-col at-col-1">
+                        <Text className={'hot-index ' + (index < 3 ? 'hot-index-hot' : '')}>
+                          {index + 1}
+                        </Text>
+                      </View>
+                      <View className="at-col at-col-8 at-col--wrap">
+                        <Text className="hot-title">{item.title}</Text>
+                        <View className="hot-footer-text">
+                          <Text>{item.comment + '回答 · ' + item.focus + '关注'}</Text>
                         </View>
                       </View>
-                      <AtLoadMore
-                        status={status}
-                        moreText="加载更多"
-                        loadingText="正在加载..."
-                        noMoreText="没有更多了"
-                      />
+                      <View className="at-col at-col-3">
+                        <Image className="hot-image" src={item.image} />
+                      </View>
                     </View>
                   );
-                })}
-              </View>
-            </ScrollView>
+                })} */}
+                  <View style={{ backgroundColor: 'red', color: 'white', height: '30vh' }}>A</View>
+                  <View style={{ backgroundColor: 'blue', color: 'white', height: '30vh' }}>B</View>
+                  <View style={{ backgroundColor: 'green', color: 'white', height: '30vh' }}>
+                    C
+                  </View>
+                  <View style={{ backgroundColor: 'orange', color: 'white', height: '30vh' }}>
+                    D
+                  </View>
+                </View>
+                <AtLoadMore
+                  onClick={this.onReachBottom}
+                  status={this.state.status}
+                  moreText="查看数据"
+                  loadingText="正在加载数据"
+                  noMoreText="没有更多了"
+                  noMoreTextStyle={{
+                    boder: 'none'
+                  }}
+                  moreBtnStyle={{
+                    boder: 'none'
+                  }}
+                />
+              </ScrollView>
+              {/* <View className="upDragBox" style={upPullStyle}>
+                <AtActivityIndicator content={upPullText}></AtActivityIndicator>
+              </View> */}
+            </View>
           </AtTabsPane>
         </AtTabs>
       </View>
